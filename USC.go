cd4c51c66140c6bb42f1d16d83a71123aa3576ba
myworldgithub <31646230+myworldgithub@users.noreply.github.com>
2018-07-10 21:22:28 +0800

    Update USC.go

--- USC.go
+++ USC.go
@@ -11,7 +11,8 @@ contract Token {
     /// @param _to The address of the recipient
     /// @param _value The amount of token to be transferred
     /// @return Whether the transfer was successful or not
-    function transfer(address _to, uint256 _value) returns (bool success) {}
+    function transfer(address _to, uint256 _value) returns (bool success) {} 
+    
 
     /// @notice send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
     /// @param _from The address of the sender
